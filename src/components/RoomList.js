7c9bac733e4381f6d3ee5ba4fd748bc6f82650af
Jessica Shepherd <jessicajade443@gmail.com>
2019-03-20 15:07:49 -0700

    checkpoint 3 create rooms completed

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -6,9 +6,22 @@ class RoomList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      rooms: []
+      rooms: [],
+      newRoomName: ''
     };
     this.roomsRef = this.props.firebase.database().ref('rooms');
+  }
+
+  handleChange(e) {
+    this.setState({ newRoomName: e.target.value });
+  }
+
+  createRoom(e){
+    e.preventDefault();
+    this.roomsRef.push({
+      name: this.state.newRoomName
+    });
+    document.getElementById('roomInput').value = '';
   }
 
   componentDidMount() {
@@ -23,7 +36,11 @@ class RoomList extends Component {
     return (
       <div className="roomList">
         <h3>Rooms:</h3>
-        { this.state.rooms.map( room => <li>{room.name}</li>) }
+        { this.state.rooms.map( room => <li>{room.name}</li>) }<br />
+        <form className="newRoom" onSubmit={(e) => this.createRoom(e)}>
+          <input type="text" id="roomInput" placeholder="Enter Room Name" onChange={(e) => this.handleChange(e)} />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     )
   }
